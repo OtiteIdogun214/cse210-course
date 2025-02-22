82454671a09c610a87779af15a7f9e7dcee501a0
OtiteIdogun214 <147813579+OtiteIdogun214@users.noreply.github.com>
2025-02-22 17:01:34 +0100

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,20 @@
 using System;
-
-class Program
+					
+public class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+	public static void Main(string[] args)
+	{
+		// Prompt the user for their first name. 
+		Console.Write("What is your first name ");
+		string f_name = Console.ReadLine();
+		
+		//Then, prompt them for their last name. 
+		Console.Write("What is your last name ");
+		string l_name = Console.ReadLine();
+
+		// Display the text back all on one line saying, 
+		// "Your name is last-name, first-name, last-name"
+		Console.WriteLine($"\nYour name is {l_name}, {f_name} {l_name}");
+		
+	}
 }
